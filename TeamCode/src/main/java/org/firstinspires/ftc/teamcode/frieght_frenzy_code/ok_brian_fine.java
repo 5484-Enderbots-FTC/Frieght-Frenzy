ca8a1ee3d798de861eaaea478f3eafd0b67038d2
MoTheMelon <ortiz97mi@gmail.com>
2021-08-21 15:36:29 -0400

    brian program time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/frieght_frenzy_code/ok_brian_fine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/frieght_frenzy_code/ok_brian_fine.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode.frieght_frenzy_code;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "teleop ff", group = "teleop")
-public class teleop_two_remotes extends LinearOpMode {
+@TeleOp(name = "ok brian", group = "teleop")
+public class ok_brian_fine extends LinearOpMode {
     //tell the code what motors and sensors you want to use (or just import the hardware teehee)
     hardwareFF robot = new hardwareFF();
 
@@ -19,13 +19,10 @@ public class teleop_two_remotes extends LinearOpMode {
         while (!isStopRequested() && opModeIsActive()) {
             //all teleop code after start button pressed goes here
 
-            //make robot wheels go brrr
-            robot.updateDrive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+            //FINE BRIAN HERES THE CONTROLS
+            robot.updateDrive(gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
 
-
-
-            //we usually add some telemetry at the end to tell us useful information during testing :)
-            telemetry.addData("left stick y value: ", gamepad1.left_stick_y);
+            telemetry.addLine("hey brian here's your freaking controls .__.");
             telemetry.update();
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/frieght_frenzy_code/var.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/frieght_frenzy_code/var.java
@@ -5,6 +5,9 @@ public class var {
     //commonly used static variables can be stored here and used by multiple programs
     //mostly for servo positions that need to be edited once in a while when a builder screws up :}
 
+    //the way you use them in other programs is by typing: var.variableName
+    //ez pz :)
+
     //example:
     public static double variable = 97;
 }

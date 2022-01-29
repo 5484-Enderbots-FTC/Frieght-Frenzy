f4567adab21f9147da85b6dedca0dd18e3181297
Tonny0414 <thomasandrewdaniels@gmail.com>
2022-01-29 09:33:14 -0500

    Update hardwareFF.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/frieght_frenzy_code/hardwareFF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/frieght_frenzy_code/hardwareFF.java
@@ -101,10 +101,8 @@ public class hardwareFF {
 
         LEDstrip = hw.get(Servo.class, "LEDstrip");
 
-        leftLimit = hw.get(TouchSensor.class, "leftLimit");
-        rightLimit = hw.get(TouchSensor.class, "rightLimit");
-        midLimit = hw.get(TouchSensor.class, "rightLimit");
-        topLimit = hw.get(TouchSensor.class, "topLimit");
+        //leftLimit = hw.get(TouchSensor.class, "midLimit");
+        midLimit = hw.get(TouchSensor.class, "midLimit");
         bottomLimit = hw.get(TouchSensor.class, "bottomLimit");
         intakeLimit = hw.get(TouchSensor.class, "intakeLimit");
 
